099703c891908130060d9aabe2c32138a2095518
Светослав Шопов <simobulgaria@gmail.com>
2026-09-11 09:11:59 +0300

    init commit

---
+++ coffee_card/lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:coffee_card/coffee_prefs.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Coffee Id', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.brown[700] ,
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text('How I like my coffee...')
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: CoffeePrefs(),
+          ),
+          Expanded(
+            child: Image.asset('assets/img/coffee_bg.jpg',
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter)
+            )
+        ],
+      )
+      
+    );
+  }
+}
